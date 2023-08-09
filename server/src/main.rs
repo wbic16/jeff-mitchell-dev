2bdda0d02409850a9b1e752fdff460e61e41ec48
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-08-08 20:37:55 -0700

    2023-08-08: Site files built to dist

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,14 +12,14 @@ use tower_http::services::ServeDir;
 // and Shuttle Turso (for an edge SQlite database)
 #[shuttle_runtime::main]
 async fn main(
-    #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
+    #[shuttle_static_folder::StaticFolder(folder = "dist")] dist_folder: PathBuf,
     #[shuttle_turso::Turso(
         addr = "libsql://healthy-lightspeed-sentinel1909.turso.io",
         token = "{secrets.DB_TURSO_TOKEN}"
     )]
     _client: Client,
 ) -> ShuttleAxum {
-    let router = Router::new().nest_service("/", ServeDir::new(static_folder));
+    let router = Router::new().nest_service("/", ServeDir::new(dist_folder));
 
     Ok(router.into())
 }
