9629b3c19598c5dde5969118bd479f0107e24535
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-09-18 19:46:19 -0700

    chore: fix blog link on home page

--- site/src/lib/pages/home.rs
+++ site/src/lib/pages/home.rs
@@ -18,12 +18,12 @@ pub fn HomePage() -> Html {
                 <section>
                     <article>
                         <h2>{ "Welcome to my site" }</h2>
-                        <p>{ "I'm a software developer in Vancouver. I love "} <a href="https://www.rust-lang.org" target="_blank"> { "Rust "}</a> { " and have embraced it as my thing. Not to be a one trick pony, I can build in HTML/CSS/JavaScript. I'm sad to admit it, but I enjoy React and can get things done with it." }</p>
+                        <p>{ "I'm a software developer in Vancouver. I love "} <a href="https://www.rust-lang.org" rel="noreferrer" target="_blank"> { "Rust "}</a> { " and have embraced it as my thing. Not to be a one trick pony, I can build in HTML/CSS/JavaScript. I'm sad to admit it, but I enjoy React and can get things done with it." }</p>
                     </article>
                     <br />
                     <article>
                         <h2>{ "Latest blog post"}</h2>
-                        <p>{ "I've posted a short "} <a href="https://blog.crusty-rustacean.dev/2023-07-31-packages-crates-modules/" target="_blank"> {" piece"}</a> {" on my blog which provides an overview of packages, crates and modules in Rust."}</p>
+                        <p>{ "I've posted a short "} <a href="https://blog.crusty-rustacean.dev/posts/2023/2023_07_31_packages_crates_modules/" rel="noreferrer" target="_blank"> {" piece"}</a> {" on my blog which provides an overview of packages, crates and modules in Rust."}</p>
                     </article>
                     <br />
                     <article>
@@ -33,12 +33,12 @@ pub fn HomePage() -> Html {
                     <br />
                     <article>
                         <h2>{ "Something I've Made" }</h2>
-                        <p><a href="https://nasaapodviewer.shuttleapp.rs" target="_blank">{ "Astronomy Picture of the Day" }</a></p>
+                        <p><a href="https://nasaapodviewer.shuttleapp.rs" rel="noreferrer" target="_blank">{ "Astronomy Picture of the Day" }</a></p>
                     </article>
                     <br />
                     <article>
                         <h2>{ "Newsletter"}</h2>
-                        <p>{"My newsletter service has been soft launched. The sign-up page can be found here: "} <a href="https://newsletter.crusty-rustacean.dev" target="_blank"> {" Crusty Rustacean Newsletter"}</a></p>
+                        <p>{"My newsletter service has been soft launched. The sign-up page can be found here: "} <a href="https://newsletter.crusty-rustacean.dev" rel="noreferrer" target="_blank"> {" Crusty Rustacean Newsletter"}</a></p>
                     </article>
                 </section>
             </main>
