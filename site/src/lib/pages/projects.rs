fcffc1378bfb3f88596d75611d151c1589ad5a4f
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-09-30 08:44:11 -0700

    chore: update dependences for site

--- site/src/lib/pages/projects.rs
+++ site/src/lib/pages/projects.rs
@@ -30,6 +30,12 @@ pub fn ProjectsPage() -> Html {
                         <p>{ "I made a Rust command line tool which takes a file with markdown (including frontmatter) as input, splits out the frontmatter, and converts the markdown body into HTML, outputting it as a file."}</p>
                         <p>{ "Source code is "} <a href="https://github.com/sentinel1909/markdown-html-rs" target="_blank">{ " here"}</a></p>
                     </article>
+                    <br />
+                    <article>
+                        <h3>{ "Rivet Head Music Diary"}</h3>
+                        <p>{ "I'm making a music diary app for myself. I want a way to track and record what I listen to during my morning and afternoon commutes to and from work.  So far, the API portion is coming togther. There is no front end yet."}</p>
+                        <p>{ "Source code is "} <a href="https://github.com/sentinel1909/rivet-head-app" target="_blank">{ " here"}</a></p>
+                    </article>
                 </section>
             </main>
             <br />
