a6b61e1cbbeef8a81acf2fd86b9e816c230a9272
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-11-27 06:59:58 -0800

    improvement: swap out Axum for Rocket in server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,17 @@
 // src/main.rs
 
 // dependencies
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
-use shuttle_axum::ShuttleAxum;
-use std::path::PathBuf;
-use tower_http::services::ServeDir;
-use tracing::info;
+use rocket::{Build, Rocket};
+use rocket::fs::{relative, FileServer};
 
-// health_check endpoint
-// serves a 200 OK response with no body
-async fn health_check() -> impl IntoResponse {
-    StatusCode::OK
+// function to create rocket instance
+fn create() -> Rocket<Build> {
+    rocket::build().mount("/", FileServer::from(relative!("dist")))
 }
 
-// main function, annotated with the Shuttle runtime
-// as well as Shuttle Static Folder (for static file hosting)
-// and Shuttle Turso (for an edge SQlite database)
 #[shuttle_runtime::main]
-async fn main() -> ShuttleAxum {
-    let router = Router::new()
-        .route("/health_check", get(health_check))
-        .nest_service("/", ServeDir::new(PathBuf::from("dist")));
+async fn main() -> shuttle_rocket::ShuttleRocket {
+    let rocket = create();
 
-    info!("Server listening on port 8000...");
-    Ok(router.into())
+    Ok(rocket.into())
 }
